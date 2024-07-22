8e509e835122b482d18440b21a68f4c08af8e805
juan-dev <juan-dev@juan-dev.juan-dev>
2024-07-22 20:41:33 -0300

    added snake comments

--- snake.go
+++ snake.go
@@ -1,10 +1,12 @@
 package main
 
+// Snake struct definition represents the snake's position on the map.
 type Snake struct {
 	x int
 	y int
 }
 
+// newSnake creates a new Snake instance with initial position (0, 0).
 func newSnake() *Snake {
 	return &Snake{
 		x: 0,
